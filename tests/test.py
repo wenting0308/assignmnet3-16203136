8c140f149486eb96bd06340e24623b7913c871db
EmmaChang <EmmaChang@localhost>
2017-02-25 14:42:12 +0000

    0225-1:create skeleton

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+'''
+Created on 2017年2月25日
+
+@author: EmmaChang
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
