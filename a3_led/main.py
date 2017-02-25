df1e1519e87c6748c456274f48580eef7bc5ce3c
EmmaChang <EmmaChang@localhost>
2017-02-25 22:06:47 +0000

    0225-3: read each line in file and decompose

--- a3_led/main.py
+++ a3_led/main.py
@@ -9,21 +9,41 @@ parser = argparse.ArgumentParser("Process input file")
 # at least one argument is required
 parser.add_argument('--input', help='a filename for caluclation', nargs='+')
 args = parser.parse_args()
+input = args.input
 
-filename = args.input
-print(type(filename))
+# read file of argument input and check if file exists
+infile = '../input_file/' + str(input[0])
 
-# read file of argument input
-infile = '../input_file/' + str(filename[0])
-if not os.path.isfile(infile):
-    print('File', infile, 'does not exist.')
-    sys.exit()
-else:
-    count=0
+try:
     fn = open(infile,'r')
-    for line in fn:
-        string=line
-        print(string)
-        count += 1
-fn.close()
+
+except IOError as e:
+    print("Error: ", e)
+
+#if file exist
+else:
+    #check if content of the file is well structured
+    #first line should be the size of LEDs
+    line = fn.readline()
+    try:
+        ledSize = int(line)
+        if ledSize <= 0:
+            raise ValueError()
+        
+    except ValueError:
+        print("Error: No proper size of LEDs!")
     
+    #if size > 0
+    else:
+        count = 0
+        while line !='':
+            
+            line = fn.readline()
+            #split command and store as list
+            line = line.replace(","," ")
+            command=line.split()
+            print(command)
+            count += 1
+        print(count)
+    fn.close()
+        
