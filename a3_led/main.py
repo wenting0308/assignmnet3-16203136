a1e029a3480138a225d8244817b2608f665e38fc
EmmaChang <EmmaChang@localhost>
2017-02-27 07:27:43 +0000

    0227-1: Change condition rule on x1>x2 or y1>y2 (swap -> ignore)

--- a3_led/main.py
+++ a3_led/main.py
@@ -4,6 +4,15 @@ import sys
 import argparse
 from pprint import pprint
 
+def parse_input():
+    """return input filename from the argument"""
+    parser = argparse.ArgumentParser("Process input file")
+    # at least one argument is required
+    parser.add_argument('--input', help='a filename for calculate total light on leds', nargs='+')
+    args = parser.parse_args()
+    input = args.input
+    return input[0]
+
 def check_is_instr(string):
     """return True if words is in the list"""
     return string in ["turn", "on", "off", "switch"]
@@ -14,17 +23,21 @@ def check_combin_instr(instr):
 
 def check_region(size, r):
     """return adjust region with valid x, y coordinate"""
-    #check if out of size
+    # check if out of size
     for i in range(4):
         if r[i] < 0:
             r[i] = 0
         elif r[i] > size-1:
             r[i] = size - 1
-    #check if start point is less than end point
+    # check if start point is less than end point
     if r[0]>r[2] or r[1]>r[3]:
-        #swap
-        r[0], r[2] = r[2], r[0]
-        r[1], r[3] = r[3], r[1]
+        ##swap
+        ##r[0], r[2] = r[2], r[0]
+        ##r[1], r[3] = r[3], r[1]
+        
+        # ignore this command:make run_led do nothing
+        # x will run for in range(0,0)
+        r[0], r[2] = 0, -1
         
     return r
 
@@ -33,19 +46,19 @@ def run_led(led, instr, r):
     xFrom, xTo = r[0], r[2]
     yFrom, yTo = r[1], r[3]
     
-    #turn on
+    # turn on
     if instr == "turnon":
         for x in range(xFrom, xTo+1):
             for y in range(yFrom, yTo+1):
                 led[x][y] = True
     
-    #turn off
+    # turn off
     elif instr == "turnoff":
         for x in range(xFrom, xTo+1):
             for y in range(yFrom, yTo+1):
                 led[x][y] = False
     
-    #switch
+    # switch
     else:
         for x in range(xFrom, xTo+1):
             for y in range(yFrom, yTo+1):
@@ -55,78 +68,81 @@ def run_led(led, instr, r):
                     led[x][y] = True
     return led
 
-# --input as optional argument for user to input a filename to process
-parser = argparse.ArgumentParser("Process input file")
-# at least one argument is required
-parser.add_argument('--input', help='a filename for caluclation', nargs='+')
-args = parser.parse_args()
-input = args.input
-
-# read file of argument input and check if file exists
-infile = '../input_file/' + str(input[0])
-try:
-    fn = open(infile,'r')
-
-except IOError as e:
-    print("Error: ", e)
-
-#if file exist
-else:
-    #check if content of the file is well structured
-    #first line should be the size of LEDs
-    line = fn.readline()
-    try:
-        ledSize = int(line)
-        if ledSize <= 0:
-            raise ValueError()
-        
-    except ValueError:
-        print("Error: No proper size of LEDs!")
+def main():
+    # --input as optional argument for user to input a filename to process
+    input = parse_input()
     
-    #if size > 0
+    # read file of argument input and check if file exists
+    infile = '../input_file/' + str(input)
+    try:
+        fn = open(infile,'r')
+    
+    except IOError as e:
+        print("Error: ", e)
+    
+    # if file exist
     else:
-        #create 2D list and initialize value to False (turn off)
-        ledLight = [ [False for i in range(ledSize)] for i in range(ledSize) ]
-
-        lineread = 0
-        while line !='':
+        # check if content of the file is well structured
+        # first line should be the size of LEDs
+        line = fn.readline()
+        try:
+            ledSize = int(line)
+            if ledSize <= 0:
+                raise ValueError()
             
-            line = fn.readline()
-            #split command and store as list
-            line = line.replace(","," ")
-            command=line.split()
-            
-            #decompose command to instruction and region
-            #initialize
-            instr = ""
-            region = [0,0,0,0]
-            i = 0
-            for s in command:
-                #first check if is instruction 
-                if check_is_instr(s):
-                    instr += s
-                #ignore "through"
-                elif s.isdigit():
-                    region[i] = int(s)
-                    i+=1
-            
-            #check if instruction after combination is valid
-            if not check_combin_instr(instr):
-                #if command not in "turnon" "turnoff" "switch" then ignore this command
-                continue
-            
-            #correct from - to region with valid coordinate
-            region = check_region(ledSize, region)
-            
-            #run command
-            ledLight = run_led(ledLight, instr, region)
-            
-            lineread += 1
-        #sum the number of lights on
-        ledOn = 0
-        for i in range(ledSize):
-            ledOn += sum(ledLight[i])
-        print("light on = ", ledOn)
-
-    fn.close()
+        except ValueError:
+            print("Error: No proper size of LEDs!")
         
+        # if size > 0
+        else:
+            # create 2D list and initialize value to False (turn off)
+            ledLight = [ [False for i in range(ledSize)] for i in range(ledSize) ]
+    
+            validCommand = 0
+            while line !='':
+                
+                line = fn.readline()
+                # split command and store as list
+                line = line.replace(","," ")
+                command=line.split()
+                
+                # decompose command to instruction and region
+                # initialize
+                instr = ""
+                region = [0,0,0,0]
+                i = 0
+                for s in command:
+                    # first check if is instruction 
+                    if check_is_instr(s):
+                        instr += s
+                    # ignore "through"
+                    elif s.isdigit():
+                        region[i] = int(s)
+                        i+=1
+                
+                # second check if instruction after combination is valid
+                if not check_combin_instr(instr):
+                    # if command not in "turnon" "turnoff" "switch" then ignore this command
+                    continue
+                
+                # correct "from - to region" with valid coordinate
+                region = check_region(ledSize, region)
+                
+                # run command
+                ledLight = run_led(ledLight, instr, region)
+                
+                validCommand += 1
+            # sum the number of lights on
+            ledOn = 0
+            for i in range(ledSize):
+                ledOn += sum(ledLight[i])
+    
+        fn.close()
+        return ledOn, validCommand
+    
+        
+    
+if __name__ == '__main__':
+    lightOn, commandRun = main()
+    print("Command run =", commandRun)
+    print("light on =", lightOn)
--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,47 @@
-'''
-Created on 2017年2月25日
 
-@author: EmmaChang
-'''
-import unittest
+import sys
+from nose.tools import *
+from a3_led.main import *
+from dis import Instruction
+
+def test_parse_input():
+    """test input filename from the argument"""
+    #eq_(parse_input(), "input_assign3_test.txt", 'Capture filename is not equal to input filename!')
+    pass
+
+def test_check_is_instr():
+    ok_(check_is_instr("on"), 'Fail checking valid instruction: turn, on, off, switch')
+    
+def test_check_combin_instr():
+    ok_(check_combin_instr("switch"), 'Fail checking valid instruction: turnon, turnoff, switch')
+    
+def test_check_region():
+    correctRegion = [0,0,2,2]
+    eq_(check_region(3, [0,0,3,3]), correctRegion, 'Region is not properly corrected.')
+    
+def test_run_led():
+    
+    ledBefore = [[False, False, False],
+                 [False, False, False],
+                 [False, False, False]]
+    
+    ledAfter = [[True, True, False],
+                 [False, False, False],
+                 [False, False, False]]
+    
+    instruction = "turnon"
+    runRegion = [0, 0, 0, 1]
+    
+    eq_(run_led(ledBefore, instruction, runRegion), ledAfter, 'Command is not properly executed on LED light.')
+    
+def test_main():
+    
+    lightOn, commandRun = main()
+    
+    eq_(lightOn, 3, 'The number on total light on is not correct.')
+    eq_(commandRun, 0, 'The number of command line read is not correct.')
 
 
-class Test(unittest.TestCase):
 
 
-    def testName(self):
-        pass
-
-
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+    
